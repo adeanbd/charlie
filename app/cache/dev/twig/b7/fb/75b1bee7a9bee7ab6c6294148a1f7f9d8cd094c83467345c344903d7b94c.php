ea4d66fbfe3f4ffed94b96f156f3bb36b5ab03a8
adeanbd <adean.contact@gmail.com>
2014-06-17 07:22:06 +0200

    First commit

---
+++ app/cache/dev/twig/b7/fb/75b1bee7a9bee7ab6c6294148a1f7f9d8cd094c83467345c344903d7b94c.php
@@ -0,0 +1,39 @@
+<?php
+
+/* charlieEditorBundle:Default:index.html.twig */
+class __TwigTemplate_b7fb75b1bee7a9bee7ab6c6294148a1f7f9d8cd094c83467345c344903d7b94c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "charlieEditorBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/charlie/EditorBundle/charlieEditorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace charlie\EditorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class charlieEditorBundle extends Bundle
+{
+}
